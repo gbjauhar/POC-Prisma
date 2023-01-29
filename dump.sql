48de76f8d91fc206bd60d41d4f97fb66042b65bf
Gabriela Jauhar <gbjauhar@gmail.com>
2023-01-29 14:36:41 -0300

    fix: update dump

--- dump.sql
+++ dump.sql
@@ -21,14 +21,60 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: _prisma_migrations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public._prisma_migrations (
+    id character varying(36) NOT NULL,
+    checksum character varying(64) NOT NULL,
+    finished_at timestamp with time zone,
+    migration_name character varying(255) NOT NULL,
+    logs text,
+    rolled_back_at timestamp with time zone,
+    started_at timestamp with time zone DEFAULT now() NOT NULL,
+    applied_steps_count integer DEFAULT 0 NOT NULL
+);
+
+
+--
+-- Name: authors; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.authors (
+    id integer NOT NULL,
+    author text NOT NULL
+);
+
+
+--
+-- Name: authors_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.authors_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: authors_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.authors_id_seq OWNED BY public.authors.id;
+
+
+--
 -- Name: books; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.books (
     id integer NOT NULL,
     title text NOT NULL,
-    genre text NOT NULL,
-    author text NOT NULL,
+    genre_id integer NOT NULL,
+    author_id integer NOT NULL,
     status text NOT NULL
 );
 
@@ -54,6 +100,43 @@ ALTER SEQUENCE public.books_id_seq OWNED BY public.books.id;
 
 
 --
+-- Name: genres; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.genres (
+    id integer NOT NULL,
+    genre text NOT NULL
+);
+
+
+--
+-- Name: genres_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.genres_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: genres_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.genres_id_seq OWNED BY public.genres.id;
+
+
+--
+-- Name: authors id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.authors ALTER COLUMN id SET DEFAULT nextval('public.authors_id_seq'::regclass);
+
+
+--
 -- Name: books id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -61,16 +144,82 @@ ALTER TABLE ONLY public.books ALTER COLUMN id SET DEFAULT nextval('public.books_
 
 
 --
+-- Name: genres id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.genres ALTER COLUMN id SET DEFAULT nextval('public.genres_id_seq'::regclass);
+
+
+--
+-- Data for Name: _prisma_migrations; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public._prisma_migrations VALUES ('9bbdebdd-d958-4151-9eaa-4894e6b1dc4e', '3fdc6fca4b7b7e43b2b207bfae98b01593e2c602f5e13b817943faac4303cfa7', '2023-01-29 13:51:06.928441-03', '20230129160118_cria_a_estrutura_da_aplicacao', NULL, NULL, '2023-01-29 13:51:06.514374-03', 1);
+INSERT INTO public._prisma_migrations VALUES ('2c5519f4-d2f7-49cc-b6a1-2e2e20c6605a', '45afb343b0ee004bc3a6b7bace53d97a8f2a8d023e91b234a5e2348e3a23fb3e', '2023-01-29 13:51:18.739456-03', '20230129165118_alterar_tabela', NULL, NULL, '2023-01-29 13:51:18.629566-03', 1);
+
+
+--
+-- Data for Name: authors; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.authors VALUES (1, 'gabo');
+INSERT INTO public.authors VALUES (2, 'jane austen');
+INSERT INTO public.authors VALUES (3, 'emily bronte');
+
+
+--
 -- Data for Name: books; Type: TABLE DATA; Schema: public; Owner: -
 --
 
+INSERT INTO public.books VALUES (2, 'cem anos de solidão', 1, 1, 'read');
+INSERT INTO public.books VALUES (3, 'orgulho e preconceito', 2, 2, 'unread');
+INSERT INTO public.books VALUES (4, 'o morro dos ventos uivantes', 3, 3, 'read');
+
+
+--
+-- Data for Name: genres; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.genres VALUES (1, 'realismo mágico');
+INSERT INTO public.genres VALUES (2, 'romance');
+INSERT INTO public.genres VALUES (3, 'drama');
+
+
+--
+-- Name: authors_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.authors_id_seq', 1, true);
 
 
 --
 -- Name: books_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.books_id_seq', 1, false);
+SELECT pg_catalog.setval('public.books_id_seq', 4, true);
+
+
+--
+-- Name: genres_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.genres_id_seq', 1, true);
+
+
+--
+-- Name: _prisma_migrations _prisma_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public._prisma_migrations
+    ADD CONSTRAINT _prisma_migrations_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: authors authors_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.authors
+    ADD CONSTRAINT authors_pkey PRIMARY KEY (id);
 
 
 --
@@ -79,6 +228,14 @@ SELECT pg_catalog.setval('public.books_id_seq', 1, false);
 
 ALTER TABLE ONLY public.books
     ADD CONSTRAINT books_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: genres genres_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.genres
+    ADD CONSTRAINT genres_pkey PRIMARY KEY (id);
 
 
 --
